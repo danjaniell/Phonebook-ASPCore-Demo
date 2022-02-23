b63953352187f2ea3ebafa8807dfd785f611b138
Jai <iaj.paragas@yahoo.com>
2022-02-23 16:23:59 +0800

    Added DbContext

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Phonebook.Data;
 
 namespace Phonebook
 {
@@ -23,6 +25,8 @@ namespace Phonebook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(opts => opts.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddControllersWithViews();
         }
 
