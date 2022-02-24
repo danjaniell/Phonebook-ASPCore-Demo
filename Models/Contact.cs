6286198bced5786b641df5bc50c9023767630116
Jai <iaj.paragas@yahoo.com>
2022-02-24 14:30:07 +0800

    Annotate DisplayName

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Phonebook.Models
@@ -6,9 +8,13 @@ namespace Phonebook.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("Email Address")]
         public string Email { get; set; }
+        [DisplayName("Mobile Number")]
         public string Mobile { get; set; }
     }
 }
